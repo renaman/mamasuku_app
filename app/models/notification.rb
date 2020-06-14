7b5d6e6be850420020433c879704e6e6c2f61e4e
renaman <n.renaman@iclound.com>
2020-06-14 13:35:19 +0900

    notificationsモデル修正

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,4 @@
 class Notification < ApplicationRecord
-	default_scope->{order(created_at: :desc)}
-
 	belongs_to :chat
 	belongs_to :visiter, class_name: 'User', foreign_key: 'visiter_id'
 	belongs_to :visited, class_name: 'User', foreign_key: 'visited_id'
